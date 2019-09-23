34a8dc41fd829db50359d804d7b871776cddacf8
Josh <joshdibble93@gmail.com>
2019-09-23 22:26:02 +0100

    Began landing layout

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import Splash from '../../images/splash1.jpg';
+import * as ROUTES from '../../constants/routes';
 
 // MUI stuff
 import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 const styles = {
   splash: {
+    position: 'relative',
     maxWidth: '100%'
+  },
+  buttons: {
+    margin: '10px auto 10px auto'
+  },
+  button: {
+    margin: '10px auto 10px auto'
   }
 };
 
 const Landing = ({ classes }) => (
-  <div>
-    <img src={Splash} className={classes.splash} />
+  <Fragment>
+    <div>
+      <img src={Splash} className={classes.splash} />
+      <div className={classes.buttons}>
+        <Button
+          className={classes.button}
+          color='primary'
+          component={Link}
+          to={ROUTES.SIGN_UP}
+        >
+          Signup
+        </Button>
+        <Button
+          className={classes.button}
+          color='primary'
+          component={Link}
+          to={ROUTES.SIGN_IN}
+        >
+          Login
+        </Button>
+      </div>
+    </div>
     <h1>LANDING</h1>
-  </div>
+  </Fragment>
 );
 
 export default withStyles(styles)(Landing);
